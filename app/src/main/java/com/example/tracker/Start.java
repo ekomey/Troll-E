2284356ad38c9cdf8ae9249dec31919541e8f06a
ekomey <49393374+ekomey@users.noreply.github.com>
2020-02-18 17:48:39 +0800

    Start button colour change and title addition

--- app/src/main/java/com/example/tracker/Start.java
+++ app/src/main/java/com/example/tracker/Start.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 
